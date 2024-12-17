6b9fbd4a66856f68e111eda066545eb7c425ae94
deepakgrandhi <deepakgrandhi@gmail.com>
2024-12-17 12:02:26 +0530

    updates

--- example/lib/axis_page_list.dart
+++ example/lib/axis_page_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tradeable_learn/models/tradeable_learn_module_model.dart';
 import 'package:tradeable_learn/tradeable_learn.dart';
 import 'package:tradeable_learn/utils/page_data.dart';
+import 'package:tradeable_learn/widgets/option_strategy_widget.dart';
 
 class AxisPageList extends StatefulWidget {
   const AxisPageList({super.key});
@@ -68,6 +69,7 @@ class _AxisPageListState extends State<AxisPageList> {
                   });
                 },
               ),
+              const OptionStrategyWidget()
             ],
           ),
         ),
